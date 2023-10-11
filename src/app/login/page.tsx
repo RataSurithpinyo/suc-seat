0abf6592c29b3ad0ccfb51f60db1a29b9e33b502
Rata Surithpinyo <rata@Ratas-MacBook-Air.local>
2023-10-11 15:41:29 +0700

    [Init] login and signup page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+
+export default function Login() {
+  return (
+    <>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-3xl font-bold leading-9 tracking-tight text-white">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label className="block text-sm font-medium leading-6 text-white">
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                // id="email"
+                // name="email"
+                // type="email"
+                // autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                Password
+              </label>
+              <div className="text-sm">
+                <a href="#" className="font-semibold text-yellow-300 hover:text-yellow-500">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-amber-200 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-amber-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-stone-200">
+          Not a member?{' '}
+          <Link href="/signup" className="font-semibold leading-6 text-yellow-300 hover:text-yellow-500">
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </div>
+  </>
+  );
+}
