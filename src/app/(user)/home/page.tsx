ad74b13e4256b0731922127812b36bffce874467
ohm <ohmkunn@hotmail.com>
2023-11-07 13:44:22 +0700

    path edited

--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -12,11 +12,7 @@ export default function page() {
       <SearchBox/>
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-2 self-center w-4/5">
         <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
-          {/* <div className="relative bg-clip-border rounded-xl overflow-hidden bg-transparent text-gray-700 shadow-none m-0 flex items-center justify-between p-6">
-            <div>
-              <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 mb-1">Projects</h6>
-            </div>
-          </div> */}
+
           <div className="p-6 px-0 pt-0 pb-2">
             <table className="w-full min-w-[640px] table-auto">
               <thead>
