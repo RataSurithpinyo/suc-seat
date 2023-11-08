3c350751fc463570d1aad8209e09983c8e52ed4b
Rata Surithpinyo <rata@Ratas-MacBook-Air.local>
2023-11-08 11:23:04 +0700

    update pages for admin

--- src/app/(LogandSign)/role/page.tsx
+++ src/app/(LogandSign)/role/page.tsx
@@ -1,7 +1,7 @@
-'use client'
-import { useRouter } from 'next/navigation'
+"use client";
+import { useRouter } from "next/navigation";
 export default function Role() {
-    const router = useRouter()
+  const router = useRouter();
   return (
     <>
       <div className="mt-20 flex min-h-full flex-1 flex-col justify-normal px-6 py-12 lg:px-8">
@@ -10,22 +10,24 @@ export default function Role() {
             Please select your role for this account
           </h2>
           <h3 className="w-[100%] mt-2 text-center text-base leading-9 tracking-tight text-white">
-            Please choose between place's owner or user. Otherwise, you will not be able to
-            create an account.
+            Please choose between place's owner or user. Otherwise, you will not
+            be able to create an account.
           </h3>
         </div>
 
         <div className="mt-8 flex justify-center">
-        <button className="text-lg mt-4 block rounded-md bg-indigo-500 hover:bg-indigo-700 px-5 py-4 text-white shadow-sm"
-        onClick={() => router.push('/createforadmin')}
-        >
-              Admin (Place's Owner)
-            </button>
-            <button className="text-lg ml-8 mt-4 block rounded-md bg-yellow-500 hover:bg-yellow-700 px-5 py-4 text-white shadow-sm"
-            onClick={() => router.push('/')} // เดี๋ยวเปลี่ยนตรงนี้เป็นหน้า home
-            >
-              User
-            </button>
+          <button
+            className="text-lg mt-4 block rounded-md bg-indigo-500 hover:bg-indigo-700 px-5 py-4 text-white shadow-sm"
+            onClick={() => router.push("/createforadmin")}
+          >
+            Admin (Place's Owner)
+          </button>
+          <button
+            className="text-lg ml-8 mt-4 block rounded-md bg-yellow-500 hover:bg-yellow-700 px-5 py-4 text-white shadow-sm"
+            onClick={() => router.push("/signup")}
+          >
+            User
+          </button>
         </div>
       </div>
     </>
--- src/app/(LogandSign)/signup/page.tsx
+++ src/app/(LogandSign)/signup/page.tsx
@@ -1,114 +1,125 @@
-'use client'
-import { useRouter } from 'next/navigation'
-
-// ที่คิดไว้ตอนนี้คือจะทำแยกเป็น 2 หน้าเลยดีไหม หน้านึงสำหรับ update จำนวนธรรมดาเลย อีกหน้าก็ update
-// information ทั่วไป
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <>
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-6 text-center text-3xl font-bold leading-9 tracking-tight text-white">
-          Create a new account
-        </h2>
-      </div>
-
-      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-white">
-                First name
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-white">
-                Last name
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-6 text-center text-3xl font-bold leading-9 tracking-tight text-white">
+            Create a new account
+          </h2>
         </div>
-          
-          <div>
-            <label htmlFor="phone" className="block text-sm font-medium leading-6 text-white">
-              Phone number
-            </label>
-            <div className="mt-2">
-              <input
-                type='tel'
-                id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                required
-                className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
 
-          <div>
-            <label className="block text-sm font-medium leading-6 text-white">
-              Username
-            </label>
-            <div className="mt-2">
-              <input
-                // id="email"
-                // name="email"
-                // type="email"
-                // autoComplete="email"
-                required
-                className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  First name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
-                Password
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Last name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="phone"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Phone number
               </label>
+              <div className="mt-2">
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                  required
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-amber-200 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-amber-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => router.push('/role')}
-            >
-              Sign up
-            </button>
-          </div>
-        </form>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-white">
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  // id="email"
+                  // name="email"
+                  // type="email"
+                  // autoComplete="email"
+                  required
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-amber-200 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-amber-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => router.push("/role")}
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
