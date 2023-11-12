becef3cb9c6491b125d7e36dd51178aecce0e3dd
NarongdaetData <6330146421@student.chula.ac.th>
2023-11-12 17:32:23 +0700

    update grpc

--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -1,13 +1,41 @@
+
 import SearchBox from '@/components/SearchBox'
+import { searchPlaces } from '@/libs/grpc-client'
 import Link from 'next/link'
-import React from 'react'
 
-export default function page() {
+import React, { useEffect, useState } from 'react';
 
+export default function Page() {
+//export default async function Page() {
+  const [placeInfo, setPlaceInfo] = useState([]);
+//----------------------------
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await searchPlaces({ name: "e" });
+  //       setPlaceInfo(data);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+//----------------------------
+//   var placeInfo :{
+//     facilities: string[],
+//     id: string,
+//     name: string,
+//     owner?: string, // Optional property
+//     capacity?: number, // Optional property
+//     availableSeat?: number // Optional property
+// }[] = await searchPlaces({ name: "e" })
+
+  console.log(Array.isArray(placeInfo))
   const mockdata = [
-    {pid:"1", name:"starback", seat:"15", facility:["chair","table"]},
-    {pid:"2", name:"sky cafe", seat:"5", facility:["macbook pro m2", "something", "someone", "somewhere"]},
-    {pid:"3", name:"threeyarn leftcity", seat:"18.25", facility:["mostsandwich","Tity's noodle"]}
+    {id:"1", name:"starback", seat:"15", facilities:["chair","table"]},
+    {id:"2", name:"skZ cafe", seat:"5", facilities:["macbook pro m2", "something", "someone", "somewhere"]},
+    {id:"3", name:"threeyarn leftcity", seat:"18.25", facilities:["mostsandwich","Tity's noodle"]}
   ]
   return (
     <div className='flex flex-col justify-center'>
@@ -36,24 +64,24 @@ export default function page() {
                 {
                 mockdata.map((Item) => (
                   
-                  <tr>
+                  <tr key={Item.id}>
                   <td className="py-3 px-5 border-b border-blue-gray-50">
                     <div className="flex items-center gap-4">
                       <p className="block antialiased font-sans text-md leading-normal text-blue-gray-900 font-bold">{Item.name}</p>
                     </div>
                   </td>
                   <td className="py-3 px-5 border-b border-blue-gray-50">
-                    <p className="pl-2 block antialiased font-sans text-md font-medium text-blue-gray-600">{Item.seat}</p>
+                    <p className="pl-2 block antialiased font-sans text-md font-medium text-blue-gray-600">{Item.facilities}</p>
                   </td>
                   <td className="py-3 px-5 border-b border-blue-gray-50">
                     <div className="w-10/12">
-                      {Item.facility.map((item) => (
+                      {Item.facilities.map((item) => (
                           <span className="mt-1 inline-flex items-center rounded-full bg-gray-100 px-2 py-1 text-md font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 ml-2  ">{item}</span>
                       ))}
                     </div>
                   </td>
                   <td className="py-3 px-5 border-b border-blue-gray-50">
-                    <Link href={`/home/${Item.pid}`} key={Item.pid}>
+                    <Link href={`/home/${Item.id}`} key={Item.id}>
                   <button className="bg-transparent hover:bg-gray-600 text-gray-600 font-medium hover:text-white py-2 px-4 border border-gray-950 hover:border-transparent rounded">
                     detail
                   </button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import Login from "./(LogandSign)/signin/page";
+import { searchPlaces } from "@/libs/grpc-client";
 export default function Home() {
+  // searchPlaces({ name: "toilet" })
   return (
     <Login/>
   );
