7f75bd502a4620e6ee23ce18e812ef5026190006
RataSurithpinyo <rataprim30026@gmail.com>
2023-11-13 19:56:19 +0700

    edit design & add logic to input fields

--- src/app/(LogandSign)/signup/page.tsx
+++ src/app/(LogandSign)/signup/page.tsx
@@ -51,7 +51,7 @@ export default function Signup() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-6 text-center text-3xl font-bold leading-9 tracking-tight text-white">
+          <h2 className="mt-20 text-center text-3xl font-bold leading-9 tracking-tight text-white">
             Create a new account
           </h2>
         </div>
--- src/app/(admin)/createforadmin/page.tsx
+++ src/app/(admin)/createforadmin/page.tsx
@@ -1,9 +1,26 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 export default function CreateForAdmin() {
+  const router = useRouter();
+  const axios = require("axios");
+  const [name, setName] = useState("");
+  const [capacity, setCapacity] = useState(0);
+  const [facilities, setFacilities] = useState<string[]>([]);
+  const handleCheckboxChange = (event:any) => {
+    const { target } = event;
+    const { name, checked, value} = target;
+    if (checked) {
+      setFacilities([...facilities, value]);
+      console.log("selected facilities: ", facilities)
+    } else {
+      setFacilities(facilities.filter((facility) => facility !== value));
+    }
+  };
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="sm:mx-auto sm:w-full sm:max-w-lg">
           <h2 className="mt-6 text-center text-3xl font-bold leading-9 tracking-tight text-white">
             Create a place
           </h2>
@@ -12,7 +29,7 @@ export default function CreateForAdmin() {
           </h3>
         </div>
 
-        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-xl">
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label
@@ -27,33 +44,14 @@ export default function CreateForAdmin() {
                   id="name"
                   name="name"
                   required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   placeholder="Enter the name you want to display for your place"
                   className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-
-            <div>
-              <label
-                htmlFor="phone"
-                className="block text-sm font-medium leading-6 text-white"
-              >
-                Phone number
-              </label>
-              <div className="mt-2">
-                <input
-                  type="tel"
-                  id="phone"
-                  name="phone"
-                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                  placeholder="xxx-xxx-xxxx"
-                  required
-                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
+            {/* <div>
               <label
                 htmlFor="facilities"
                 className="block text-sm font-medium leading-6 text-white"
@@ -68,7 +66,7 @@ export default function CreateForAdmin() {
                   className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>
+            </div> */}
 
             <div>
               <label
@@ -79,13 +77,93 @@ export default function CreateForAdmin() {
               </label>
               <div className="mt-2">
                 <input
-                  min={0}
+                  type="number"
+                  id="capacity"
+                  name="capacity"
+                  placeholder="Enter a number"
+                  required
+                  value={capacity}
+                  onChange={(e) => setCapacity(parseInt(e.target.value))}
+                  className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="capacity"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Available Seats
+              </label>
+              <div className="mt-2">
+                <input
                   type="number"
                   id="capacity"
                   name="capacity"
                   placeholder="Enter a number"
                   className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="facilities"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Facilities
+              </label>
+              <div className="mt-2 text-white text-sm font-medium leading-6 flex items-center">
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    value="24/7"
+                    name="24/7"
+                    className="mr-2"
+                    onChange={handleCheckboxChange}
+                  />
+                  24/7
+                </label>
+                <label className="flex items-center ml-4">
+                  <input
+                    type="checkbox"
+                    value="Free Wi-Fi"
+                    name="Free Wi-Fi"
+                    className="mr-2"
+                  />{" "}
+                  Free Wi-Fi
+                </label>
+                <label className="flex items-center ml-4">
+                  <input
+                    type="checkbox"
+                    value="Free Drinks & Snacks"
+                    name="Free Drinks & Snacks"
+                    className="mr-2"
+                    onChange={handleCheckboxChange}
+                  />
+                  Free Drinks & Snacks
+                </label>
+                <label className="flex items-center ml-4">
+                  <input
+                    type="checkbox"
+                    value="Power Plug"
+                    name="Power Plug"
+                    className="mr-2"
+                    onChange={handleCheckboxChange}
+                  />
+                  Power Plug
+                </label>
+                <label className="flex items-center ml-4">
+                  <input
+                    type="checkbox"
+                    value="Toilet"
+                    name="Toilet"
+                    className="mr-2"
+                    onChange={handleCheckboxChange}
+                  />
+                  Toilet
+                </label>
               </div>
             </div>
 
@@ -103,7 +181,7 @@ export default function CreateForAdmin() {
                     value="true"
                     name="reservable"
                     className="mr-2"
-                  />{" "}
+                  />
                   YES
                 </label>
                 <label className="flex items-center ml-4">
@@ -112,7 +190,7 @@ export default function CreateForAdmin() {
                     value="false"
                     name="reservable"
                     className="mr-2"
-                  />{" "}
+                  />
                   NO
                 </label>
               </div>
@@ -122,7 +200,6 @@ export default function CreateForAdmin() {
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-amber-200 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-amber-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                // onClick={() => alert("Created Successfully.")}
               >
                 Create Place
               </button>
