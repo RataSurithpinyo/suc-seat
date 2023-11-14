ca6bb6a242288dadda4db3176ea99c9613a4dabc
NarongdaetData <6330146421@student.chula.ac.th>
2023-11-15 01:29:40 +0700

    seach done kod keng

--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -2,6 +2,7 @@
 import SearchBox from '@/components/SearchBox'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react';
+// import Multiselect from '../../../../src/components/Multiselect.tsx';
 interface PlaceListInterface {
   place: {
     facilities: string[];
@@ -33,19 +34,18 @@ export default function Page() {
   }
   const axios = require("axios");
   const [placeListInfo, setPlaceListInfo] = useState(mockdata);
-  // const [searchInfo, setSeachInfo] = useState("");
+  const [query, setQuery] = useState("")
+   const [searchInfo, setSeachInfo] = useState("");
   // const [FacilityInfo, setFacilityInfo] = useState("");
 
   const url = "http://localhost:8080"; 
 
       // put the fetchData inside the effect
       async function fetchData() {
-        const name = "test"
         const response = await axios.post(
           `${url}/search`,
           {
-            name
-            //placeListInfo
+            name:query
           },
           {
             headers: {
@@ -65,6 +65,7 @@ export default function Page() {
           })),
         };
         setPlaceListInfo(mappedData)
+        console.log("mapped",query);
         console.log("mapped",placeListInfo);
       }
 
@@ -78,7 +79,31 @@ export default function Page() {
    
   return (
     <div className='flex flex-col justify-center'>
-      <SearchBox Facilities={placeListInfo} />
+      {/* <SearchBox Facilities={placeListInfo} /> */}
+      <div className="flex flex-col md:flex-row gap-3 mt-5 mb-6 xl:mx-80 lg:mx-52 md:mx-32 sm:mx-8 text justify-center">
+        <div className="flex flex-auto">
+            <input type="text" placeholder="Search for Coworking Spaces"
+			    className="flex flex-grow w-full md:w-4/6 px-3 h-10 rounded-l border-2 border-gray-950 focus:outline-none focus:border-gray-950"
+			    value={query}
+          onChange={(e) => 
+            setQuery(e.target.value)
+          }
+          />
+            <button type="submit" className="bg-gray-950 h-10 text-white rounded-r px-2 md:px-3 py-0 md:py-1"
+            onClick={fetchData}
+            >Search</button>
+        </div>
+    
+        {/* <select multiple id="pricingType" name="pricingType"
+		    // className="flex flex-initial h-10 border-2 border-gray-950 focus:outline-none focus:border-gray-950 text-gray-950 rounded px-2 md:px-3 py-0 md:py-1 tracking-wider"
+        className='block w-full p-3 border border-gray-300 rounded-sm cursor-pointer focus:outline-none' >
+		    <option value="All" selected>All</option>
+		    <option value="Freemium">Freemium</option>
+		    <option value="Free">Free</option>
+		    <option value="Paid">Paid</option>
+	    </select> */}
+      {/* <Multiselect Facilities={placeListInfo}/> */}
+    </div>
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-2 self-center w-4/5">
         <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2">
           <div className="p-6 px-0 pt-0 pb-2">
@@ -123,7 +148,7 @@ export default function Page() {
                     {/* <Link href={`/home/${Item.id}`} key={Item.id}> */}
                     <Link href={`/home/${PlaceItem.id}?name=${PlaceItem.name}&availableSeat=${PlaceItem.availableSeat}&facilities=${PlaceItem.facilities}&capacity=${PlaceItem.capacity}`} key={PlaceItem.id} >
                   <button className="bg-transparent hover:bg-gray-600 text-gray-600 font-medium hover:text-white py-2 px-4 border border-gray-950 hover:border-transparent rounded"
-                   onClick={fetchData}
+                   
                 >
                     detail
                   </button>
